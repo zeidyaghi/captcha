e7e456a463f6e9b4ce48fc81121ca20d579b5828
zeid <74204600+zeidyaghi@users.noreply.github.com>
2025-07-11 15:28:58 +0300

    Delete alphanumeric directory

--- alphanumeric/js/_vti_cnf/jquery-captcha.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|04 Oct 2024 13:04:20 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|zeidcaptcha.awardspace.biz/z/examples/alphanumeric/signalphanumericMM.htm zeidcaptcha.awardspace.biz/z/examples/alphanumeric/signalphanumericAM1.htm zeidcaptcha.awardspace.biz/z/examples/alphanumeric/signjq.htm zeidcaptcha.awardspace.biz/z/examples/alphanumeric/signalphanumericAM.htm
-vti_author:SR|OSAMA-PC\\OSAMA
-vti_modifiedby:SR|OSAMA-PC\\OSAMA
-vti_timecreated:TR|04 Oct 2024 13:04:20 -0000
-vti_cacheddtm:TX|04 Oct 2024 13:04:20 -0000
-vti_filesize:IR|5369
